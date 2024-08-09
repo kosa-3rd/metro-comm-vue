6e645f8897c7998cdea84d7a6cbaa20ed3b44330
kim-hoyoung <happy3sk@naver.com>
2024-08-09 17:37:55 +0900

    mypage 생성 및 upper-compo 적용

--- src/components/content/MyPageCmp.vue
+++ src/components/content/MyPageCmp.vue
@@ -1,37 +1,81 @@
 <template>
-    <div>
+  <div>
+      <!-- 상단 컴포넌트 -->
       <div id="upper-compo">
-            <div id="upper-compo-left" class="w-6">
-                <button @click="goPrev">
-                    <img src="../../assets/left_arrow.png" style="width: 1.5rem; height: 1.5rem" />
-                </button>
-            </div>
-            <div id="upper-compo-center">
-                <h3 id="sign-info" class="text-xl font-semibold text-center mb-4">마이페이지</h3>
-            </div>
-            <div id="upper-compo-right" class="w-6"></div>
-        </div>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    methods: {
-        goPrev() {
+          <div id="upper-compo-left" class="w-6 mb-4 mt-4">
+              <button @click="goPrev">
+                  <img src="../../assets/left_arrow.png" style="width: 1.5rem; height: 1.5rem" />
+              </button>
+          </div>
+          <div id="upper-compo-center">
+              <h3 id="sign-info" class="text-xl font-semibold text-center mb-4 mt-4">마이페이지</h3>
+          </div>
+          <div id="upper-compo-right" class="w-6"></div>
+      </div>
+      
+      <!-- 사용자 정보 섹션 -->
+      <div id="user-info" class="flex justify-between items-center px-4 py-4 border-bd">
+          <div>
+              <h2 class="text-2xl font-semibold">username</h2>
+              <p class="text-gray-500">email@naver.com</p>
+          </div>
+          <button class="text-gray-500 font-semibold" @click="logout">로그아웃</button>
+      </div>
+
+      <!-- 메뉴 항목들 -->
+      <ul class="menu-list">
+          <li class="menu-item border-b py-4 px-4">내가 쓴 글</li>
+          <li class="menu-item border-b py-4 px-4">비밀번호 변경</li>
+          <li class="menu-item border-b py-4 px-4">회원 탈퇴</li>
+      </ul>
+
+<!-- 하단 광고 섹션 -->
+<div class="px-4 py-4">
+    <img src="../../assets/ad.png" alt="에듀윌은 합격이다" class="w-2/3 h-auto mx-auto"/>
+</div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+      goPrev() {
           this.$router.push('/');
-        },
-    },
-  };
-  </script>
-  
-  <style scoped>
+      },
+      logout() {
+          // 로그아웃 로직을 여기에 추가
+          console.log('로그아웃');
+      },
+  },
+};
+</script>
+
+<style scoped>
 #upper-compo {
-    justify-content: space-between;
-    margin: 0 2rem 0 2rem;
-    @apply flex mb-4 pb-0;
+  justify-content: space-between;
+  margin: 0 2rem 0 2rem;
+  @apply flex mb-4 mt-4 pb-0;
+  border-bottom: 1px solid #e5e5e5; /* 선 추가 */
+  border-top: 1px solid #e5e5e5
+}
+
+#user-info {
+  @apply flex justify-between items-center px-4 py-4 border-b pb-36;
+}
+
+.menu-list {
+  @apply list-none p-0;
+}
+
+.menu-item {
+  @apply border-b py-4 px-4 cursor-pointer;
+}
+
+.menu-item:hover {
+  background-color: #f5f5f5;
 }
 
 #upper-compo * {
-    /* background: red; */
-}</style>
-  
+  /* background: red; */
+}
+</style>
