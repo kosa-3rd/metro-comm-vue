13b9496cd3f06523933a45b436dd0748207d68c3
rjsah09 <rjsah09@naver.com>
2024-08-13 14:38:32 +0900

    로그인 기능 수정

--- src/store/user-store.js
+++ src/store/user-store.js
@@ -1,5 +1,7 @@
 import { defineStore } from 'pinia';
 import { ref, computed } from 'vue';
+import axios from 'axios';
+import router from '@/router';
 
 export const useUserStore = defineStore(
     'users',
@@ -13,26 +15,44 @@ export const useUserStore = defineStore(
             return user.value.token != null;
         });
 
-        const getUser = function () {
+        const getEmail = computed(() => {
             return user.value.email;
-        };
+        });
 
-        const getAuth = function () {
+        const getAuth = computed(() => {
             return user.value.token;
-        };
+        });
 
         const login = function (info) {
             user.value.email = info.email;
             user.value.token = info.token;
         };
 
-        const logout = function () {
-            sessionStorage.removeItem('users');
-            user.value = null;
+        const logout = async function () {
+            try {
+                await axios.delete('/api/users', {
+                    data: {
+                        email: getEmail.value,
+                    },
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: getAuth.value, // computed 속성의 .value 사용
+                    },
+                });
+
+                sessionStorage.removeItem('users');
+                user.value.email = null;
+                user.value.token = null;
+                router.push('/');
+            } catch (err) {
+                console.log(getAuth.value); // 디버깅을 위한 토큰 출력
+                console.log(err); // 오류 로그 출력
+            }
+
             return sessionStorage.getItem('users') == null;
         };
 
-        return { user, authenticated, login, getUser, getAuth, logout };
+        return { user, authenticated, login, getEmail, getAuth, logout };
     },
     {
         persist: {
